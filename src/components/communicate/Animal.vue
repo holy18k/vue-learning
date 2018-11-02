14f49bb2a2b98857581bf74c2b3e819480743429
heming <jujumouse@126.com>
2018-11-02 18:44:29 +0800

    提交更新

---
+++ src/components/communicate/Animal.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div class="list-group">
+    <Lion :vals="msg"></Lion>
+  </div>
+</template>
+
+<script>
+  import Lion from './Lion'
+
+  export default {
+    components: {
+      Lion
+    },
+    name: 'Animal',
+    data () {
+      return {
+        msg: '父组件的数据,将传递给子组件11155551'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/communicate/Lion.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{vals}}
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{ //父组件传值 可以是一个数组，对象
+      vals:{
+        type:String,//类型为字符窜
+        default:"123" //可以设置默认值
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
